4bab1797189dd99599284fb5365eba1669c0f8b8
papanlesat <papanlesat@gmail.com>
2025-04-29 19:35:33 +0700

    .

--- config/db.go
+++ config/db.go
@@ -6,7 +6,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-// DbConfiguration builds a MySQL DSN from env vars.
 func DbConfiguration() string {
 	user := viper.GetString("DB_USER")
 	password := viper.GetString("DB_PASSWORD")
@@ -14,7 +13,6 @@ func DbConfiguration() string {
 	port := viper.GetString("DB_PORT")
 	name := viper.GetString("DB_NAME")
 
-	// add any extra params you need here
 	return fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, password, host, port, name,
